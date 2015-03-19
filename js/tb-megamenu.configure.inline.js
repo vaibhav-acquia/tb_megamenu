afca4723dcd546c640687199125a07489c40a6b3
Michael Ngo <michaelngodh@gmail.com>
2015-03-19 09:23:05 +0700

    Remove file which contains JS inline

--- js/tb-megamenu.configure.inline.js
+++
@@ -1,8 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  
-  "use strict";
-  
-  Drupal.TBMegaMenu = Drupal.TBMegaMenu || {};
-  Drupal.TBMegaMenu.ajax_link = "' . " + drupalSettings.tb_megamenu.clean_url + ". '";
-  
-})(jQuery, Drupal, drupalSettings);
