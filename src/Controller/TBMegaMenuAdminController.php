04e53f921b0ad0266eaedc20608df097bfc6165b
Michael Ngo <michaelngodh@gmail.com>
2015-04-02 09:18:45 +0700

    Create menu callback to save configuration of TB MegaMenu

--- src/Controller/TBMegaMenuAdminController.php
+++ src/Controller/TBMegaMenuAdminController.php
@@ -9,9 +9,11 @@ namespace Drupal\tb_megamenu\Controller;
 
 use Drupal\Core\Url;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\tb_megamenu\TBMegaMenuBuilder;
+use Symfony\Component\HttpFoundation\Response;
 
 class TBMegaMenuAdminController extends ControllerBase {
-  
+
   /**
    * This is page callback. Listing mega menus.
    */
@@ -43,11 +45,11 @@ class TBMegaMenuAdminController extends ControllerBase {
     }
     // Prepare label for headers.
     $header = array(
-      'menu-name' => t('Menu Name'), 
+      'menu-name' => t('Menu Name'),
       'menu-title' => t('Menu Title'),
       'menu-operations' => t('Operations')
     );
-    
+
     $table = array(
       '#theme' => 'table',
       '#header' => $header,
@@ -57,4 +59,61 @@ class TBMegaMenuAdminController extends ControllerBase {
     );
     return $table;
   }
+  
+  /**
+   * This is menu callback. Save configuration of TB Mega Menu.
+   */
+  public function saveConfiguration() {
+    $action = isset($_POST['action']) ? $_POST['action'] : NULL;
+    switch ($action) {
+      case 'load':
+        $block = TBMegaMenuBuilder::renderBlock($_POST['menu_name']);
+        return new Response(drupal_render($block));
+        break;
+      case 'save':
+        $menu_config = isset($_POST['menu_config']) ? $_POST['menu_config'] : NULL;
+        $block_config = isset($_POST['block_config']) ? $_POST['block_config'] : NULL;
+        $menu_name = isset($_POST['menu_name']) ? $_POST['menu_name'] : NULL;
+        if ($menu_config && $menu_name) {
+          $tb_megamenu = db_select('tb_megamenus', 't')
+                  ->fields('t')
+                  ->condition('menu_name', $menu_name)
+                  ->execute()
+                  ->fetchObject();
+          if ($tb_megamenu) {
+            db_update('tb_megamenus')
+                    ->fields(array('menu_config' => $menu_config, 'block_config' => $block_config))
+                    ->condition('menu_name', $menu_name)
+                    ->execute();
+          }
+          else {
+            db_insert('tb_megamenus')
+              ->fields(array(
+                'menu_name' => $menu_name,
+                'block_config' => $block_config,
+                'menu_config' => $menu_config
+              ))->execute();
+          }
+        }
+        break;
+      case 'load_block':
+        $block_id = isset($_POST['block_id']) ? $_POST['block_id'] : NULL;
+        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
+        $showblocktitle = isset($_POST['showblocktitle']) ? $_POST['showblocktitle'] : NULL;
+        if ($block_id) {
+          $argument = array(
+            '#theme' => 'tb_megamenu_block',
+            '#block_id' => $block_id, 
+            '#section' => 'backend', 
+            '#showblocktitle' => $showblocktitle
+          );
+          $content = drupal_render($argument);
+          return new Response(json_encode(array('content' => $content, 'id' => $id)));
+        }
+        return new Response('');
+        break;
+    }
+    exit;
+  }
+
 }
