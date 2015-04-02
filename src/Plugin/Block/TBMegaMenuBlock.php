cb5257816edc8b48b4215333cd042a252f50b946
Michael Ngo <michaelngodh@gmail.com>
2015-04-02 10:04:34 +0700

    Build content for TB Mega Menu blocks

--- src/Plugin/Block/TBMegaMenuBlock.php
+++ src/Plugin/Block/TBMegaMenuBlock.php
@@ -8,7 +8,6 @@
 namespace Drupal\tb_megamenu\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\tb_megamenu\TBMegaMenuBuilder;
 
 /**
  * Provides blocks which belong to TB Mega Menu.
@@ -17,7 +16,7 @@ use Drupal\tb_megamenu\TBMegaMenuBuilder;
  * @Block(
  *   id = "tb_megamenu_menu_block",
  *   admin_label = @Translation("TB Mega Menu"),
- *   category = @Translation("TB MegaMenus"),
+ *   category = @Translation("TB Mega Menu"),
  *   deriver = "Drupal\tb_megamenu\Plugin\Derivative\TBMegaMenuBlock"
  * )
  *
@@ -28,36 +27,11 @@ class TBMegaMenuBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $menu_name = $this->getDerivativeId();
-    
     // It's used to test.
-    $content = array(
-      '#theme' => 'tb_megamenu_block',
-      '#menu_name' => 'main_menu',
-      '#block_key' => 'tb_megamenu_menu_block:footer',
-      '#section' => 'frontend',
-      '#showblocktitle' => 1,
+    return array(
+      '#theme' => 'tb_megamenu',
+      '#menu_name' => $this->getDerivativeId(),
+      '#attached' => array('library' => array('tb_megamenu/theme.tb_megamenu'))
     );
-//
-//    // Adjust the menu tree parameters based on the block's configuration.
-//    $level = $this->configuration['level'];
-//    $depth = $this->configuration['depth'];
-//    $parameters->setMinDepth($level);
-//    // When the depth is configured to zero, there is no depth limit. When depth
-//    // is non-zero, it indicates the number of levels that must be displayed.
-//    // Hence this is a relative depth that we must convert to an actual
-//    // (absolute) depth, that may never exceed the maximum depth.
-//    if ($depth > 0) {
-//      $parameters->setMaxDepth(min($level + $depth - 1, $this->menuTree->maxDepth()));
-//    }
-//
-//    $tree = $this->menuTree->load($menu_name, $parameters);
-//    $manipulators = array(
-//      array('callable' => 'menu.default_tree_manipulators:checkAccess'),
-//      array('callable' => 'menu.default_tree_manipulators:generateIndexAndSort'),
-//    );
-//    $tree = $this->menuTree->transform($tree, $manipulators);
-//    $this->menuTree->build($tree);
-    return $content;
   }
 }
