119e22a383548b818d6b5e38b487af16468d1f35
CG Monroe <cgmonroe@563114.no-reply.drupal.org>
2017-12-03 18:40:52 -0500

    Menu block now rebuilt automatically if menu or megamenu config is changed

--- src/Plugin/Block/TBMegaMenuBlock.php
+++ src/Plugin/Block/TBMegaMenuBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\tb_megamenu\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\tb_megamenu\TBMegaMenuBuilder;
 
@@ -21,11 +22,18 @@ use Drupal\tb_megamenu\TBMegaMenuBuilder;
 class TBMegaMenuBlock extends BlockBase {
 
   /**
+   * Current theme name;
+   *
+   * @var string
+   */
+  private $themeName;
+
+  /**
    * {@inheritdoc}
    */
   public function build() {
     $menu_name = $this->getDerivativeId();
-    $theme_name = \Drupal::service('theme.manager')->getActiveTheme()->getName();
+    $theme_name = $this->getThemeName();
     $menu = TBMegaMenuBuilder::getMenus($menu_name, $theme_name);
     if ($menu === NULL) {
       return [];
@@ -36,11 +44,6 @@ class TBMegaMenuBlock extends BlockBase {
       '#block_theme' => $theme_name,
       '#attached' => ['library' => ['tb_megamenu/theme.tb_megamenu']],
       '#post_render' => ['tb_megamenu_attach_number_columns'],
-      '#cache' => [
-        'contexts' => [
-          'route.menu_active_trails:' . $this->getDerivativeId(),
-        ],
-      ],
     ];
   }
 
@@ -60,5 +63,43 @@ class TBMegaMenuBlock extends BlockBase {
     $rebuild_form['cache']['max_age']['#default_value'] = 0;
     return $rebuild_form;
   }
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    // Rebuild block when menu or config changes
+    $configName = "{$this->getDerivativeId()}__{$this->getThemeName()}";
+    $cacheTags = parent::getCacheTags();
+    $cacheTags[] = 'config:system.menu.' . $this->getDerivativeId();
+    $cacheTags[] = 'config:tb_megamenu.menu_config.' . $configName;
+    return $cacheTags;
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    // ::build() uses MenuLinkTreeInterface::getCurrentRouteMenuTreeParameters()
+    // to generate menu tree parameters, and those take the active menu trail
+    // into account. Therefore, we must vary the rendered menu by the active
+    // trail of the rendered menu.
+    // Additional cache contexts, e.g. those that determine link text or
+    // accessibility of a menu, will be bubbled automatically.
+    $menu_name = $this->getDerivativeId();
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route.menu_active_trails:' . $menu_name]);
+  }
+
+  /**
+   * Get the current Theme Name
+   *
+   * @return string
+   * The current theme name.
+   */
+  public function getThemeName() {
+    if (!isset($this->themeName)) {
+      $this->themeName = \Drupal::service('theme.manager')->getActiveTheme()->getName();
+    }
+    return $this->themeName;
+  }
 }
+
