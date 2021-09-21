07e918acb712ae504d90b166e546336e5d09706e
Michael Girgis <knaffles@gmail.com>
2021-09-21 15:27:13 -0500

    Updates for the 9/22 TB MegaMenu release.

--- templates/tb-megamenu-item.tpl.php
+++ templates/tb-megamenu-item.tpl.php
@@ -1,20 +1,20 @@
 <?php if(isset($item['link']['href'])) : ?>
   <li <?php print $attributes;?> class="<?php print $classes;?>">
     <?php if ($item['link']['href'] == "<nolink>"): ?>
-      <a href="#" class="tb_nolink <?php if (isset($item['link']['localized_options']['attributes']['class'])) { print implode(' ', $item['link']['localized_options']['attributes']['class']); } ?>">
+      <a href="#" class="tb_nolink <?php if (isset($item['link']['localized_options']['attributes']['class'])) { print check_plain(implode(' ', $item['link']['localized_options']['attributes']['class'])); } ?>">
         <?php if(!empty($item_config['xicon'])) : ?>
           <i class="<?php print $item_config['xicon'];?>"></i>
         <?php endif;?>
-        <?php print t($item['link']['link_title']);?>
+        <?php print t(check_plain($item['link']['link_title']));?>
       </a>
     <?php elseif ($item['link']['href'] == "<separator>"): ?>
-      <div class="tb_separator <?php if (isset($item['link']['localized_options']['attributes']['class'])) { print implode(' ', $item['link']['localized_options']['attributes']['class']); } ?>"></div>
+      <div class="tb_separator <?php if (isset($item['link']['localized_options']['attributes']['class'])) { print check_plain(implode(' ', $item['link']['localized_options']['attributes']['class'])); } ?>"></div>
     <?php else: ?>
-      <a href="<?php print url($item['link']['href'], $item['link']['options']);?>" <?php echo drupal_attributes($item['link']['#attributes']); ?>>
+      <a href="<?php print check_url(url($item['link']['href'], $item['link']['options']));?>" <?php echo drupal_attributes($item['link']['#attributes']); ?>>
         <?php if(!empty($item_config['xicon'])) : ?>
           <i class="<?php print $item_config['xicon'];?>"></i>
         <?php endif;?>
-        <?php print t($item['link']['title']);?>
+        <?php print t(check_plain($item['link']['title']));?>
         <?php if($submenu && $block_config['auto-arrow']) :?>
           <span class="caret"></span>
         <?php endif;?>
