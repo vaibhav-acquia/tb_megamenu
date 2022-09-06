dc82826908a6446d688a1af1a007bdfa22b10570
Michael Girgis <michael.girgis@bounteous.com>
2022-09-06 15:45:56 -0500

    3274658: Remove leftover console.log in dist file.

--- dist/js/frontend.js
+++ dist/js/frontend.js
@@ -423,8 +423,6 @@ var TBMegaMenu = function () {
       document.addEventListener('click', function (event) {
         if (!event.target.closest('.tbm') && _this.navParent.classList.contains('tbm--mobile-show')) {
           _this.closeMenu();
-
-          console.log('oops i did it again');
         }
       });
       document.addEventListener('focusin', function (event) {
